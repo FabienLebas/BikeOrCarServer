13dbddd96528523f309a1d905a18133336b26e20
Fabien Lebas <fabien.lebas@decathlon.com>
2018-02-04 07:30:14 +0100

    getCityName OK

---
+++ queries/google.js
@@ -0,0 +1,26 @@
+const fetch = require("node-fetch");
+const REACT_APP_googleAPIKey = process.env.googleAPIKey;
+
+function getCityName(latitude, longitude){
+  return fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${REACT_APP_googleAPIKey}`)
+    .then(result => result.json())
+    .then(returnedData => {
+      const today = new Date();
+      const formattedDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+      const result={
+        city: filterCity(returnedData),
+        country: returnedData.results[1].formatted_address.split(",")[1],
+        date: formattedDate
+      };
+      return result;
+    })
+    .catch(error => {
+      console.warn("Google API error : " + error);
+    })
+}
+
+function filterCity(googleResult){
+  return googleResult.results[0].address_components.filter(element => element.types[0] === "locality")[0].long_name;
+}
+
+module.exports = getCityName;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const getCityName = require("./queries/google");
+
+const port = process.env.PORT || 4000;
 
 app.listen(port, function () {
   console.log("Server listening on port:" + port);
+});
+
+app.post("/getCityName", function (request, result) {
+  getCityName("50.6793842", "3.1836387")
+    .then(object => {
+      result.json(JSON.stringify(object));
+    })
 });
 
 app.get("/", function (request, result) {
